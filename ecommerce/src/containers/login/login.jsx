50a5e93e0bc41aa71036df37964fee9eb2fc6925
Adrian M.A <adrian@neurocadi.es>
2020-07-18 20:44:10 +0200

    Connected to endpoint

--- ecommerce/src/containers/login/login.jsx
+++ ecommerce/src/containers/login/login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import "./login.css";
 
 class Login extends React.Component {
@@ -6,29 +7,39 @@ class Login extends React.Component {
         super(props);
 
         this.state = {
-            name: "",
-            surnames: "",
-            address: "",
-            phone: "",
-            document: "",
             email: "",
-            password: ""
+            password: "",
+            registered: false
         }
+            // Se enlaza, bind, la función al estado
+        // this.login = this.login.bind(this);
 
-    //     this.createRegister = this.createRegister.bind(this);
+    };
+
+    handleChange = (ev) =>{
+        this.setState({[ev.target.name]: ev.target.type === 'string' ? +ev.target.value : ev.target.value}); 
     }
 
-    // async register() {
+    handleSubmit = event =>{
+        event.preventDefault();
+        let body = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        console.log(body)
 
+        axios.post("http://localhost:3001/users/login", body)
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(error => console.error(error))
+    }
 
-
-    //     try {
-            
-    //     } catch (error) {
-            
-    //         console.error(error)
+    // this.history.block((location, action) => {
+    //     if (input.value !== '') {
+    //         return '¿Seguro que quiere abandonar la página sin completar el formulario?';
     //     }
-    // }
+    // });
 
     render() {
         return(
@@ -39,10 +50,10 @@ class Login extends React.Component {
                     </div>
                     <h3>Entra para conocer nuestras variedades</h3>
                     <div id="formInputs">
-                        <form action="/" method="post">
-                            <input type="email" name="email" id="emailInput" placeholder="Introduce tu email"/>
-                            <input type="password" name="password" id="passwordInput" placeholder="Introduce tu contraseña" />
-                            <button type="submit">Regístrate</button>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="email" name="email" id="emailInput" value={this.state.email} onChange={this.handleChange} placeholder="Introduce tu email"/>
+                            <input type="password" name="password" id="passwordInput" value={this.state.password} onChange={this.handleChange} placeholder="Introduce tu contraseña" />
+                            <button type="submit">Log in</button>
                         </form>
                     </div>
                 </div>
