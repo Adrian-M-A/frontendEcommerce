8295df0685ddc7415b70f27cd5d8563968296706
JessicagaRojas <65513403+JessicagaRojas@users.noreply.github.com>
2020-07-23 00:47:07 +0200

    pruebas con axios para traer productos

--- ecommerce/src/containers/pedidoMenu/pedidomenu.jsx
+++ ecommerce/src/containers/pedidoMenu/pedidomenu.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import axios from "axios";
 import "./pedidomenu.css";
 import Subheader from "../Subheader/subheader.jsx";
 
 
 class pedidomenu extends React.Component {
 
+    state = { 
+        products: [],
+        status: null
+    };
+
+    componentWillMount(){  {/*Ciclo de vida para que cargue primero esto*/}
+        this.getProducts();
+    }
+
+    getProducts = () => {
+        axios.get("http://localhost:3001/products")
+             .then( res => {
+                 this.setState({
+                     products: res.data.products,
+                     status: 'succes'
+
+                 });
+                 console.log(this.state);
+             });
+    }
+   
+
     render() {
         return (
             <div id="pedidomenucontainer">
                 <Subheader />
+                <h1>Holi</h1>
+
 
             </div>
 
