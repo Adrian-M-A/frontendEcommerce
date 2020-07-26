e355b4238aaa4da059a3855f0871d2018c54827f
Adrian M.A <adrian@neurocadi.es>
2020-07-26 20:56:36 +0200

    Package json fixed

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from "react-redux";
-import store from "./services/redux/store.js";
+import {Provider} from 'react-redux';
+import store from './services/redux/store.js';
 
 ReactDOM.render(
   <React.StrictMode>
