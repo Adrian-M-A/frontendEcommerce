15c31ae8f1f893d1a39c603817755b6f49de1d7b
Adrian M.A <adrian@neurocadi.es>
2020-07-28 15:50:49 +0200

    Fixing errors

--- ecommerce/src/containers/header/headerFija.jsx
+++ ecommerce/src/containers/header/headerFija.jsx
@@ -10,18 +10,11 @@ class headerFija extends React.Component {
             <div id="header">
                 <NavLink to="/" id="logoheader"></NavLink>
                 <nav id="menu">
-<<<<<<< HEAD
-                    <div id="logoheader"></div>
-                    <NavLink to="/menu" className="menubuttons" id="orderbutton">Pedido</NavLink>
-                    <NavLink to="/checkout" className="menubuttons" id="cartbutton">Cesta</NavLink>
-                    <div id="logocart"></div>
-=======
                     <NavLink to="/products" className="menubuttons" id="orderbutton">Pedido</NavLink>
                     <NavLink to="/login" className="menubuttons" id="loginButtonHeader">Login</NavLink>
                     {/* <button className="menubuttons" id="cartbutton">Cesta</button> */}
                     <NavLink to="/checkout" id="logocart"></NavLink>
                     {/* <div id="logocart"></div> */}
->>>>>>> develop
                 </nav>
             </div>
          );
